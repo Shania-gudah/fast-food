4e780179cd82f95c64ebc2a60a6efddb755c2b4b
Shania-gudah <115345157+Shania-gudah@users.noreply.github.com>
2022-10-08 12:03:00 +0300

    Add files via upload
    firs commit

---
+++ FAST FOOD.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        Dim username As String
+        Dim password As String
+        username = Txtusername.Text
+        password = txtpassword.Text
+        If (Txtusername.Text = ("Delicious") And txtpassword.Text = ("pass254")) Then
+            MessageBox.Show("Login Successful", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("ERROR", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        Form2.Show()
+        Me.Hide()
+
+
+    End Sub
+End Class
